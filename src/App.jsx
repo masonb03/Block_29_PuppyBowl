6e4ea4ad987ee72a1ac4102a2c88f0f0074f3114
Mason <bevinsm355@gmail.com>
2025-04-22 20:00:29 -0400

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-import {Routes, Route} from 'react'
+import {Routes, Route, Link, Navigate, useNavigate} from 'react'
 
 function App() {
+const [token, setToken] = useState(()=> localStorage.getItem("token"));
+const [player, setPlayer] = useState([]);
+const [favPlayer, setFavPlayer] = useState(null);
+const navigate = useNavigate();
+
+useEffect(() =>{
+  if(token) {
+    localStorage.setItem("token", token);
+  } else {
+    localStorage.removeItem("token");
+  }
+}, [token]);
+
 
   return (
     <>
+    <Routes>
+      
+    </Routes>
     </>
   )
 }
